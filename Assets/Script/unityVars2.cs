1dc91e9260bf8db57d1660089b5842be388b615f
saci zakaria <sacideveloppeur@gmail.com>
2024-03-07 12:42:00 +0100

    public GameObject go

--- Assets/Script/unityVars2.cs
+++ Assets/Script/unityVars2.cs
@@ -7,7 +7,7 @@ public class unityVars2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        public GameObject go = new GameObject();
     }
 
     // Update is called once per frame
