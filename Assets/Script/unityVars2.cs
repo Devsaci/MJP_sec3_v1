d480ced9d07ece824bfe97c7c2d8118778c101f2
saci zakaria <sacideveloppeur@gmail.com>
2024-03-07 18:28:35 +0100

    public Vector3 goPos;

--- Assets/Script/unityVars2.cs
+++ Assets/Script/unityVars2.cs
@@ -7,6 +7,7 @@ public class unityVars2 : MonoBehaviour
 
     public GameObject go;
     public Transform goTransforme;
+    public Vector3 goPos;
 
     // Start is called before the first frame update
     void Start()
